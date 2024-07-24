fbdd32bfa87259335e1d3b8932e38424ac8f32f5
PrincyChauhan <chauhanprincee7@gmail.com>
2024-07-24 19:17:23 +0530

    create basic routes

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.get("/api/contact", (req, res) => {
-    res.status(200).json({ message: "Get all contacts" });
-});
+app.use("/api/contacts", require("./routes/contactRoutes"));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
