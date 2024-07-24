8936d8457fa539c27247bb51dffa58a03639e17b
PrincyChauhan <chauhanprincee7@gmail.com>
2024-07-24 19:40:22 +0530

    express throw an error

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -2,16 +2,18 @@ const getContacts = (req, res) => {
     res.status(200).json({ message: "Get all contacts" });
 }
 
-
 const createContact = (req, res) => {
+    console.log(req.body);
+    const { name, email, phone } = req.body;
+    if (!name || !email || !phone) {
+        res.status(400);
+        throw new Error("Please fill in all fields");
+    }
     res.status(201).json({ message: "Create contact" });
 }
-
-
 const getContact = (req, res) => {
     res.status(200).json({ message: `Get contact for ${req.params.id}` });
 }
-
 
 const updateContact = (req, res) => {
     res.status(200).json({ message: `Update contact for ${req.params.id}` });
