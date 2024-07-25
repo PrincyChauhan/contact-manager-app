5bb66e665e5b53bf219ec46c91461a4d48727567
PrincyChauhan <chauhanprincee7@gmail.com>
2024-07-25 22:02:31 +0530

    add acessToken

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const registerUser = asyncHandler(async(req, res) => {
     const { username, email, password } = req.body;
@@ -35,7 +36,25 @@ const registerUser = asyncHandler(async(req, res) => {
 
 
 const loginUser = asyncHandler(async(req, res) => {
-    res.json({ message: "Login user" });
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("Please fill in all fields");
+    }
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id
+            }
+        }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1m" })
+        res.json({ accessToken });
+    } else {
+        res.status(400);
+        throw new Error("Invalid email or password");
+    }
 });
 
 const currentUser = asyncHandler(async(req, res) => {
